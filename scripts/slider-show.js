5aeac13b5318ff51c1d8e767ee239e9dfb0fff35
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-09-13 10:45:55 -0300

    updated proj (slider feat): add some news function to move slider

--- scripts/slider-show.js
+++ scripts/slider-show.js
@@ -2,6 +2,13 @@ export default class SliderShow {
    constructor(slider, slider_content) {
       this.slider = document.querySelector(slider);
       // this.slider_content = document.querySelector(slider_content);
+
+      this.position = {
+         final: 0,
+         start: 0,
+         movement: 0,
+         new_pos: 0,
+      };
    }
 
    getImagesInAPI() {
@@ -52,17 +59,62 @@ export default class SliderShow {
 
    clientEvent() {
       this.slider.addEventListener('mousedown', this.startSlider);
+      this.slider.addEventListener('touchstart', this.startSlider);
+
+      this.slider.addEventListener('mouseup', this.endMoveSlider);
    }
 
    startSlider(ev) {
-      console.log(ev.type);
+      let type_ev;
+
+      if (ev.type === 'mousedown') {
+         ev.preventDefault();
+         this.position.start = ev.clientX;
+         type_ev = 'mousemove';
+      } else {
+         type_ev = 'touchmove';
+      }
+
+      this.slider.addEventListener(type_ev, this.onMoveSlider);
+   }
+
+   onMoveSlider(ev) {
+      const client_pos = ev.type === 'mousemove' ? ev.clientX : '';
+
+      const move_cli = this.updatedPosition(client_pos);
+
+      this.moveSlider(move_cli);
+   }
+
+   endMoveSlider(ev) {
+      const type_action = ev.type === 'mouseup' ? 'mousemove' : 'touchmove';
+      this.slider.removeEventListener(type_action, this.onMoveSlider);
+
+      this.position.final = this.position.new_pos;
+   }
+
+   updatedPosition(clientX) {
+      this.position.movement = (this.position.start - clientX) * 1.1;
+
+      return this.position.final - this.position.movement;
+   }
+
+   moveSlider(trans) {
+      this.position.new_pos = trans;
+      const slider_content = this.slider.querySelector('.__slider-items');
+
+      slider_content.style.transform = `translateX(${trans}px)`;
+      console.log(trans);
    }
 
    eventBind() {
       this.startSlider = this.startSlider.bind(this);
+      this.onMoveSlider = this.onMoveSlider.bind(this);
+      this.endMoveSlider = this.endMoveSlider.bind(this);
    }
 
    init() {
+      this.eventBind();
       this.getImagesInAPI();
       this.clientEvent();
       return this;
